c7cddbe65448e187232d832d95e91d1991453b50
saucecat <jiangchun.lee@gmail.com>
2018-04-13 00:03:47 +0800

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='pydqc',
 	packages=['pydqc'],
 	package_data={'pydqc': ['templates/*.txt']},
-	version='0.1',
+	version='0.1.0',
 	description='python automatic data quality check',
 	author='SauceCat',
 	author_email='jiangchun.lee@gmail.com',
